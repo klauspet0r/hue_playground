d28f41b681f0cddf782500c6fc7445245517a88e
klauspeter <kreuzbuerger@gmail.com>
2020-10-23 19:44:37 +0200

    added \n to output

--- link_rotary_decoder_to_light.py
+++ link_rotary_decoder_to_light.py
@@ -1,21 +1,21 @@
 import phue
 import sys
 from gpiozero import Button
+import RPi.GPIO as GPIO
+import threading
 from time import sleep
 
-button = Button(24)  #TODO:
+button = Button(24)  # TODO:
 
 bridge = phue.Bridge(str(sys.argv[1]))
-
-
 
 bridge.connect()
 
 api_response = bridge.get_api()
 
-print('Press button to get Lampnames')
+print('Press button to get Lampnames\n')
 
-while(True):
+while True:
 
     if button.is_pressed:
         print('Number of connected Lamps: ' + str(len(api_response['lights'])) + '\n')
@@ -24,15 +24,6 @@ while(True):
         print('+++++++++++++++ \n')
         for key, value in api_response['lights'].items():
             print("#" + key + ' ' + value['name'])
-            #for entry in api_response['lights'][key].items():
-                #print(entry['name'])
+            # for entry in api_response['lights'][key].items():
+            # print(entry['name'])
         break
-
-
-
-
-
-
-
-
-
