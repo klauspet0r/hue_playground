d11ced2cbd74aa5d1852cf6e1249fd3ea722930b
klauspeter <kreuzbuerger@gmail.com>
2020-10-23 16:49:15 +0200

    Update link_rotary_decoder_to_light.py
    added first button action

--- link_rotary_decoder_to_light.py
+++ link_rotary_decoder_to_light.py
@@ -1,5 +1,9 @@
 import phue
 import sys
+from gpiozero import Button
+from time import sleep
+
+button = Button(24)  #TODO:
 
 bridge = phue.Bridge(str(sys.argv[1]))
 
@@ -9,14 +13,23 @@ bridge.connect()
 
 api_response = bridge.get_api()
 
-print('Number of connected Lamps: ' + str(len(api_response['lights'])) + '\n')
+while(True):
 
-print('All lamp names: \n')
-print('+++++++++++++++ \n')
-for key, value in api_response['lights'].items():
-    print("#" + key + ' ' + value['name'])
-    #for entry in api_response['lights'][key].items():
-        #print(entry['name'])
+    if button.is_pressed:
+        print('Number of connected Lamps: ' + str(len(api_response['lights'])) + '\n')
+
+        print('All lamp names: \n')
+        print('+++++++++++++++ \n')
+        for key, value in api_response['lights'].items():
+            print("#" + key + ' ' + value['name'])
+            #for entry in api_response['lights'][key].items():
+                #print(entry['name'])
+
+    if button.is_released:
+        break
+
+
+
 
 
 
