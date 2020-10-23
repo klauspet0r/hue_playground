9d33c8ca24e6bdbcd2b48c7f54225d5751e35999
klauspeter <kreuzbuerger@gmail.com>
2020-10-23 15:31:07 +0200

    Update link_rotary_decoder_to_light.py
    connect and get some api info

--- link_rotary_decoder_to_light.py
+++ link_rotary_decoder_to_light.py
@@ -5,5 +5,16 @@ bridge = phue.Bridge('192.168.2.100')
 
 bridge.connect()
 
-print(bridge.get_api())
+api_response = bridge.get_api()
+
+print('length of api dictionary is: ' + str(len(api_response)))
+
+print('list of the keys in the api dict:  ' + str(api_response.keys()))
+
+#print(api_response.keys().[1])
+
+print('lights are: ' + str(api_response['lights'].keys()))
+
+
+
 
