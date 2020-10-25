6c8ea16118fef80311f8024fe6e6d16fb7ba11ca
klauspeter <kreuzbuerger@gmail.com>
2020-10-25 13:07:08 +0100

    Update gpio_callback_fun.py

--- gpio_callback_fun.py
+++ gpio_callback_fun.py
@@ -3,7 +3,16 @@ from time import sleep
 
 
 def rotation_decode(Enc_A):
-        print("something happend on the decoder")
+
+    # read both of the switches
+    Switch_A = GPIO.input(Enc_A)
+    Switch_B = GPIO.input(Enc_B)
+
+    if (Switch_A == 1) and (Switch_B == 0):
+        print("clockwise")
+
+    elif (Switch_A == 1) and (Switch_B == 1):
+        print("counterclockwise")
 
 counter = 0
 
@@ -22,7 +31,6 @@ GPIO.setup(Enc_B, GPIO.IN)
 
 # setup an event detection thread for the A encoder switch
 GPIO.add_event_detect(Enc_A, GPIO.RISING, callback=rotation_decode, bouncetime=2) # bouncetime in mSec
-
 
 while(True):
 
