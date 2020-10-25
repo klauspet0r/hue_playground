c188c34d2ac94a2ca1289f5474cb181d6a0e2fd8
klauspeter <kreuzbuerger@gmail.com>
2020-10-25 22:30:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import RPi.GPIO as GPIO
 
 try:
 
-    while True:
+    rotary_encoder = RotaryEncoder(22, 23)
 
-        rotary_encoder = RotaryEncoder(22, 23)
+    while True:
 
         while rotary_encoder.get_value_change():
             print(str(rotary_encoder.decoder_counter))
