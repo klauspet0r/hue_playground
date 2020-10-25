0552e32d4e7179efebe68da3d031e84b782bcb4d
klauspeter <kreuzbuerger@gmail.com>
2020-10-25 19:38:21 +0100

    Update gpio_callback_fun.py

--- gpio_callback_fun.py
+++ gpio_callback_fun.py
@@ -1,55 +1,60 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
+# GPIO Ports
+clk = 23  # Encoder input A: input GPIO 23 (active high)
+dt = 24  # Encoder input B: input GPIO 24 (active high)
 
-def rotation_decode(Enc_A):
+gpio_list = [clk, dt]
 
+
+def init_rotary_decoder():
+    GPIO.setwarnings(True)
+
+    # Use the Raspberry Pi BCM pins
+    GPIO.setmode(GPIO.BCM)
+
+    # define the Encoder switch inputs
+    GPIO.setup(gpio_list, GPIO.IN)  # pull-ups are too weak, they introduce noise
+
+    # setup an event detection thread for the A encoder switch
+    GPIO.add_event_detect(clk, GPIO.RISING, callback=decode_rotation, bouncetime=2)  # bouncetime in mSec
+
+    return
+
+
+def decode_rotation(clk):
     # read both of the switches
-    Switch_A = GPIO.input(Enc_A)
-    Switch_B = GPIO.input(Enc_B)
+    CLK = GPIO.input(clk)
+    DT = GPIO.input(dt)
 
-    if (Switch_A == 1) and (Switch_B == 0):
+    sleep(0.002)  # extra 2 mSec de-bounce time
+
+    if (CLK == 1) and (DT == 0):
         print("clockwise")
-        while Switch_B == 0:
-            Switch_B = GPIO.input(Enc_B)
+        while DT == 0:
+            DT = GPIO.input(dt)
         # now wait for B to drop to end the click cycle
-        while Switch_B == 1:
-            Switch_B = GPIO.input(Enc_B)
+        while DT == 1:
+            DT = GPIO.input(dt)
         return
 
-    elif (Switch_A == 1) and (Switch_B == 1):
+    elif (CLK == 1) and (DT == 1):
         print("counterclockwise")
         # A is already high, wait for A to drop to end the click cycle
-        while Switch_A == 1:
-            Switch_A = GPIO.input(Enc_A)
-        return
-    
-    else: # discard all other combinations
+        while CLK == 1:
+            CLK = GPIO.input(clk)
         return
 
-counter = 0
+    else:  # discard all other combinations
+        return
 
-# GPIO Ports
-Enc_A = 23  # Encoder input A: input GPIO 23 (active high)
-Enc_B = 24  # Encoder input B: input GPIO 24 (active high)
 
-GPIO.setwarnings(True)
-
-# Use the Raspberry Pi BCM pins
-GPIO.setmode(GPIO.BCM)
-
-# define the Encoder switch inputs
-GPIO.setup(Enc_A, GPIO.IN) # pull-ups are too weak, they introduce noise
-GPIO.setup(Enc_B, GPIO.IN)
-
-# setup an event detection thread for the A encoder switch
-GPIO.add_event_detect(Enc_A, GPIO.RISING, callback=rotation_decode, bouncetime=2) # bouncetime in mSec
-
-while(True):
+while (True):
 
     try:
+        init_rotary_decoder()
         sleep(1)
 
-    except KeyboardInterrupt: # Ctrl-C to terminate the program
+    except KeyboardInterrupt:  # Ctrl-C to terminate the program
         GPIO.cleanup()
-
