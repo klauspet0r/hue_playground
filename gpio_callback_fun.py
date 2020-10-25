6ce62805c7c679d9c88c65e03a1e47c16aa5af94
klauspeter <kreuzbuerger@gmail.com>
2020-10-25 20:24:51 +0100

    Update gpio_callback_fun.py

--- gpio_callback_fun.py
+++ gpio_callback_fun.py
@@ -42,7 +42,7 @@ def decode_rotation(clk):
 
     if (CLK == 1) and (DT == 0):
         list_index += 1
-        if list_index > len(list_of_rooms):
+        if list_index > len(list_of_rooms) - 1:
             list_index = 0
         print('list index: ' + str(list_index) + 'room: ' + list_of_rooms[list_index])
         while DT == 0:
@@ -55,7 +55,7 @@ def decode_rotation(clk):
     elif (CLK == 1) and (DT == 1):
         list_index -= 1
         if list_index < 0 or list_index > len(list_of_rooms):
-            list_index = len(list_of_rooms)
+            list_index = len(list_of_rooms) - 1
         print('list index: ' + str(list_index) + ' room: ' + list_of_rooms[list_index])
         while CLK == 1:
             CLK = GPIO.input(clk)
