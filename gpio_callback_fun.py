a527c36f4125dc65c2b7cd27245fe358cf68554e
klauspeter <kreuzbuerger@gmail.com>
2020-10-25 22:00:10 +0100

    Update gpio_callback_fun.py

--- gpio_callback_fun.py
+++ gpio_callback_fun.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 from time import sleep
+import os
 
 # GPIO Ports
 clk = 22  # Encoder input A: input GPIO 23 (active high)
@@ -9,6 +10,10 @@ list_index = 10
 
 
 # gpio_list = [clk, dt]
+
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def init_rotary_decoder():
@@ -44,6 +49,7 @@ def decode_rotation(clk):
         list_index += 1
         if list_index > len(list_of_rooms) - 1:
             list_index = 0
+        cls()
         print('#: ' + str(list_index) + ' Raum: ' + list_of_rooms[list_index])
         while DT == 0:
             DT = GPIO.input(dt)
@@ -56,6 +62,7 @@ def decode_rotation(clk):
         list_index -= 1
         if list_index < 0 or list_index > len(list_of_rooms):
             list_index = len(list_of_rooms) - 1
+        cls()
         print('#: ' + str(list_index) + ' Raum: ' + list_of_rooms[list_index])
         while CLK == 1:
             CLK = GPIO.input(clk)
