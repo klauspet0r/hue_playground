25176b1341ea5578d12cedf8710c387113a8aeda
klauspeter <kreuzbuerger@gmail.com>
2020-10-27 19:34:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,49 @@ from RotaryEncoder import RotaryEncoder
 from time import sleep
 import RPi.GPIO as GPIO
 
+clk = 22
+dt = 23
+
+
+def decode_rotation(clk):
+    decoder_counter = 0
+    sleep(0.002)  # debounce time
+
+    CLK = GPIO.input(clk)
+    DT = GPIO.input(dt)
+
+    sleep(0.002)  # extra 2 mSec de-bounce time
+
+    if (CLK == 1) and (DT == 0):
+        print(str('self.decoder_counter += 1'))
+        decoder_counter += 1
+        while DT == 0:
+            DT = GPIO.input(dt)
+
+        while DT == 1:
+            DT = GPIO.input(dt)
+        value_changed = True
+        return
+
+    elif (CLK == 1) and (DT == 1):
+        print(str('self.decoder_counter -= 1'))
+        decoder_counter -= 1
+        while CLK == 1:
+            CLK = GPIO.input(clk)
+        value_changed = True
+        return
+
+    else:  # discard all other combinations
+        value_changed = False
+        return
+
+
 try:
 
-    rotary_encoder = RotaryEncoder(22, 23)
+    rotary_encoder = RotaryEncoder(clk, dt, decode_rotation)
 
     while True:
-
-        while rotary_encoder.get_value_change():
-            print(str(rotary_encoder.decoder_counter))
-            sleep(0.01)
+        sleep(0.01)
 
 except KeyboardInterrupt:  # Ctrl-C to terminate the program
     GPIO.cleanup()
