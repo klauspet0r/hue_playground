25e9b3dce4699193a64f746f36b5ce44826abcbf
klauspeter <kreuzbuerger@gmail.com>
2020-10-27 19:06:58 +0100

    Update RotaryEncoder.py
    decode_rotation callback function needs now to be passed as an argument to the constructor

--- RotaryEncoder.py
+++ RotaryEncoder.py
@@ -8,7 +8,7 @@ class RotaryEncoder:
     clk = None
     dt = None
 
-    def __init__(self, clk, dt):
+    def __init__(self, clk, dt, decode_rotation):
 
         self.value_changed = False
         self.decoder_counter = 0
@@ -25,41 +25,41 @@ class RotaryEncoder:
         GPIO.setup(dt, GPIO.IN)
 
         # setup an event detection thread for the A encoder switch
-        GPIO.add_event_detect(clk, GPIO.RISING, callback=self.decode_rotation, bouncetime=2)  # bouncetime in mSec
+        GPIO.add_event_detect(clk, GPIO.RISING, callback=decode_rotation, bouncetime=2)  # bouncetime in mSec
 
         return
 
-    def decode_rotation(self, clk):
-
-        sleep(0.002)  # debounce time
-
-        CLK = GPIO.input(self.clk)
-        DT = GPIO.input(self.dt)
-
-        sleep(0.002)  # extra 2 mSec de-bounce time
-
-        if (CLK == 1) and (DT == 0):
-            print(str('self.decoder_counter += 1'))
-            self.decoder_counter += 1
-            while DT == 0:
-                DT = GPIO.input(self.dt)
-
-            while DT == 1:
-                DT = GPIO.input(self.dt)
-            value_changed = True
-            return
-
-        elif (CLK == 1) and (DT == 1):
-            print(str('self.decoder_counter -= 1'))
-            self.decoder_counter -= 1
-            while CLK == 1:
-                CLK = GPIO.input(self.clk)
-            value_changed = True
-            return
-
-        else:  # discard all other combinations
-            value_changed = False
-            return
+    # def decode_rotation(self, clk):
+    #
+    #     sleep(0.002)  # debounce time
+    #
+    #     CLK = GPIO.input(self.clk)
+    #     DT = GPIO.input(self.dt)
+    #
+    #     sleep(0.002)  # extra 2 mSec de-bounce time
+    #
+    #     if (CLK == 1) and (DT == 0):
+    #         print(str('self.decoder_counter += 1'))
+    #         self.decoder_counter += 1
+    #         while DT == 0:
+    #             DT = GPIO.input(self.dt)
+    #
+    #         while DT == 1:
+    #             DT = GPIO.input(self.dt)
+    #         value_changed = True
+    #         return
+    #
+    #     elif (CLK == 1) and (DT == 1):
+    #         print(str('self.decoder_counter -= 1'))
+    #         self.decoder_counter -= 1
+    #         while CLK == 1:
+    #             CLK = GPIO.input(self.clk)
+    #         value_changed = True
+    #         return
+    #
+    #     else:  # discard all other combinations
+    #         value_changed = False
+    #         return
 
     def get_value_change(self):
         return self.value_changed
