0ac3a4eb4d41ae4fb0542cc1f495309700fe8a74
klauspeter <kreuzbuerger@gmail.com>
2020-10-27 19:41:29 +0100

    Update RotaryEncoder.py

--- RotaryEncoder.py
+++ RotaryEncoder.py
@@ -29,37 +29,5 @@ class RotaryEncoder:
 
         return
 
-    # def decode_rotation(self, clk):
-    #
-    #     sleep(0.002)  # debounce time
-    #
-    #     CLK = GPIO.input(self.clk)
-    #     DT = GPIO.input(self.dt)
-    #
-    #     sleep(0.002)  # extra 2 mSec de-bounce time
-    #
-    #     if (CLK == 1) and (DT == 0):
-    #         print(str('self.decoder_counter += 1'))
-    #         self.decoder_counter += 1
-    #         while DT == 0:
-    #             DT = GPIO.input(self.dt)
-    #
-    #         while DT == 1:
-    #             DT = GPIO.input(self.dt)
-    #         value_changed = True
-    #         return
-    #
-    #     elif (CLK == 1) and (DT == 1):
-    #         print(str('self.decoder_counter -= 1'))
-    #         self.decoder_counter -= 1
-    #         while CLK == 1:
-    #             CLK = GPIO.input(self.clk)
-    #         value_changed = True
-    #         return
-    #
-    #     else:  # discard all other combinations
-    #         value_changed = False
-    #         return
-
     def get_value_change(self):
         return self.value_changed
