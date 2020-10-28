9c72d49daacf598b643cae48446a399c511d1424
klauspeter <kreuzbuerger@gmail.com>
2020-10-28 22:14:14 +0100

    Update EncodingFun.py

--- EncodingFun.py
+++ EncodingFun.py
@@ -1,6 +1,10 @@
 # -*- coding: iso-8859-1 -*-
-string_mit_scharfem_s = u'Straße'
 
+import os
+import locale
+os.environ["PYTHONIOENCODING"] = "utf-8"
+myLocale=locale.setlocale(category=locale.LC_ALL, locale="de_DE.UTF-8");
 
+string_mit_scharfem_s = 'Straße'
 
-print(string_mit_scharfem_s)
+print(string_mit_scharfem_s.encode('utf-8', errors='ignore'))
