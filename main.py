4f2039d512d51393d6464f579c4d1f9273116ab4
klauspeter <kreuzbuerger@gmail.com>
2020-10-30 22:37:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,41 +15,53 @@ import phue
 
 clk = 22  # gpio pin for clk
 dt = 23  # gpio pin for dt
-decoder_counter = 0
-lamplist = []
+list_index = 0
+itemlist = []
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def decode_rotation(clk):
-    sleep(0.002)  # debounce time
+    global list_index
 
-    global decoder_counter
-
+    sleep(0.002)  # extra 2 mSec de-bounce time
+    # read both of the switches
     CLK = GPIO.input(clk)
     DT = GPIO.input(dt)
 
     sleep(0.002)  # extra 2 mSec de-bounce time
 
     if (CLK == 1) and (DT == 0):
-        decoder_counter += 1
-        print(str(decoder_counter))
+        list_index += 1
+        if list_index > len(itemlist) - 1:
+            list_index = 0
+        cls()
+        print('\n')
+        print('********************************************')
+        print('#: ' + str(list_index + 1) + ' Raum: ' + itemlist[list_index])
+        print('********************************************')
         while DT == 0:
             DT = GPIO.input(dt)
-
+        # now wait for B to drop to end the click cycle
         while DT == 1:
             DT = GPIO.input(dt)
-        value_changed = True
         return
 
     elif (CLK == 1) and (DT == 1):
-        decoder_counter -= 1
-        print(str(decoder_counter))
+        list_index -= 1
+        if list_index < 0 or list_index > len(itemlist):
+            list_index = len(itemlist) - 1
+        cls()
+        print('\n')
+        print('********************************************')
+        print('#: ' + str(list_index + 1) + ' Raum: ' + itemlist[list_index])
+        print('********************************************')
         while CLK == 1:
             CLK = GPIO.input(clk)
-        value_changed = True
         return
 
     else:  # discard all other combinations
-        value_changed = False
         return
 
 
@@ -70,7 +82,7 @@ try:
 
     for key, value in api_response['groups'].items():
         print(str(value['name']) + '\n')
-        lamplist.append(value['name'])
+        itemlist.append(value['name'])
 
     rotary_encoder = RotaryEncoder(clk, dt, decode_rotation)
 
