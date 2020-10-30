1239cdc6b8fc4331b0d009eba29f6cb31431515c
klauspeter <kreuzbuerger@gmail.com>
2020-10-30 22:58:18 +0100

    Update RotaryEncoder.py

--- RotaryEncoder.py
+++ RotaryEncoder.py
@@ -8,12 +8,13 @@ class RotaryEncoder:
     clk = None
     dt = None
 
-    def __init__(self, clk, dt, decode_rotation):
+    def __init__(self, clk, dt, sw, decode_rotation):
 
         self.value_changed = False
         self.decoder_counter = 0
         self.clk = clk
         self.dt = dt
+        self.sw = sw
 
         GPIO.setwarnings(True)
 
@@ -23,6 +24,7 @@ class RotaryEncoder:
         # define the Encoder switch inputs
         GPIO.setup(clk, GPIO.IN)  # pull-ups are too weak, they introduce noise
         GPIO.setup(dt, GPIO.IN)
+        GPIO.setup(sw, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
         # setup an event detection thread for the A encoder switch
         GPIO.add_event_detect(clk, GPIO.RISING, callback=decode_rotation, bouncetime=2)  # bouncetime in mSec
