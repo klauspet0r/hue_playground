bc12ed830b09a86ec9e338c3d3566527eaf77fce
klauspeter <kreuzbuerger@gmail.com>
2020-11-09 15:57:34 +0100

    Update OLED_SSD1306_fun.py

--- OLED_SSD1306_fun.py
+++ OLED_SSD1306_fun.py
@@ -35,22 +35,24 @@ while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-    # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+    # # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
     cmd = "hostname -I | cut -d\' \' -f1"
     IP = subprocess.check_output(cmd, shell = True )
-    cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
-    CPU = subprocess.check_output(cmd, shell = True )
-    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
-    MemUsage = subprocess.check_output(cmd, shell = True )
-    cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
-    Disk = subprocess.check_output(cmd, shell = True )
+    # cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
+    # CPU = subprocess.check_output(cmd, shell = True )
+    # cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
+    # MemUsage = subprocess.check_output(cmd, shell = True )
+    # cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
+    # Disk = subprocess.check_output(cmd, shell = True )
 
     # Write two lines of text.
 
-    draw.text((x, top),       "IP: " + str(IP),  font=font, fill=255)
-    draw.text((x, top+8),     str(CPU), font=font, fill=255)
-    draw.text((x, top+16),    str(MemUsage),  font=font, fill=255)
-    draw.text((x, top+25),    str(Disk),  font=font, fill=255)
+
+
+    draw.text((x, top),       'HUE HUB',  font=font, fill=255)
+    draw.text((x, top+8),     'IP: '  + str(IP).replace('\n', ''), font=font, fill=255)
+    draw.text((x, top+16),    'WORK IN',  font=font, fill=255)
+    draw.text((x, top+25),    'PROGRESS',  font=font, fill=255)
 
     # Display image.
     disp.image(image)
