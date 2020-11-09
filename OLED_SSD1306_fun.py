baac45eb9cbf3bdf47f0e6530009051452a4a4ce
klauspeter <kreuzbuerger@gmail.com>
2020-11-09 15:59:08 +0100

    Update OLED_SSD1306_fun.py

--- OLED_SSD1306_fun.py
+++ OLED_SSD1306_fun.py
@@ -15,29 +15,28 @@ disp.begin()
 disp.clear()
 disp.display()
 
-
 width = disp.width
 height = disp.height
 image = Image.new('1', (width, height))
 
 draw = ImageDraw.Draw(image)
 
-draw.rectangle((0,0,width,height), outline=0, fill=0)
+draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
 padding = -2
 top = padding
-bottom = height-padding
+bottom = height - padding
 x = 0
-font = ImageFont.truetype('VCR_OSD_MONO_1.001.ttf', 8)
+font_size = 12
+font = ImageFont.truetype('VCR_OSD_MONO_1.001.ttf', font_size)
 
 while True:
-
     # Draw a black filled box to clear the image.
-    draw.rectangle((0,0,width,height), outline=0, fill=0)
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
     # # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
     cmd = "hostname -I | cut -d\' \' -f1"
-    IP = subprocess.check_output(cmd, shell = True )
+    IP = subprocess.check_output(cmd, shell=True)
     # cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
     # CPU = subprocess.check_output(cmd, shell = True )
     # cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
@@ -45,17 +44,11 @@ while True:
     # cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
     # Disk = subprocess.check_output(cmd, shell = True )
 
-    # Write two lines of text.
-
-
-
-    draw.text((x, top),       'HUE HUB',  font=font, fill=255)
-    draw.text((x, top+8),     'IP: '  + str(IP).replace('\n', ''), font=font, fill=255)
-    draw.text((x, top+16),    'WORK IN',  font=font, fill=255)
-    draw.text((x, top+25),    'PROGRESS',  font=font, fill=255)
+    draw.text((x, top), 'HUE HUB', font=font, fill=255)
+    draw.text((x, top + font_size), 'IP: ' + str(IP).replace('\n', ''), font=font, fill=255)
+    draw.text((x, top + font_size * 2), 'WORK IN PROGRESS', font=font, fill=255)
 
     # Display image.
     disp.image(image)
     disp.display()
     time.sleep(.1)
-
